1eb9784c64b6fa41475233bc17d69ece748d1790
D4rk Gh057 <danielfuelpaz@outlook.com>
2023-01-31 22:22:16 -0500

    Se comprueba la funcionalidad de la conexion

--- JS/conexion.js
+++ JS/conexion.js
@@ -1,11 +1,22 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host: 'bwlzwusixbbsd7oqhmvd-mysql.services.clever-cloud.com',
-  user: 'uvtrtmhl1kh3bgym',
-  password: 'srpWdxh10ffLwT5WsyY0'
+var con = mysql.createConnection({
+  host: 'bxjlkcq3c4ybesmfzmw6-mysql.services.clever-cloud.com',
+  user: 'unuf0c853htqddan',
+  password: 'I3JJi91nrBQDXYcS2rTW',
+  database: 'bxjlkcq3c4ybesmfzmw6'
 });
 
-connection.connect((err) => {
+con.connect(function (err) {
   if (err) throw err;
-  console.log('Conexión satisfactoria')
+  console.log("Connected!");
+  var sql = "INSERT INTO Entrenador (Nombre, Region) VALUES ?";
+  var values = [
+    ['Marilyn', 'Alola'],
+    ['Majo', 'Galar'],
+    ['Miguel', 'Paldea']
+  ];
+  con.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
 });
