364bb19581706e51332f056cca2d9a1d7e9c896c
D4rk Gh057 <danielfuelpaz@outlook.com>
2023-02-04 17:09:22 -0500

    se corrigen algunos errores del CSS y las referencias a berries

--- JS/pokemonBerries.js
+++ JS/pokemonBerries.js
@@ -1,5 +1,6 @@
 async function fetchBerry(id) {
   const res = await fetch(`https://pokeapi.co/api/v2/berry/${id}/`);
   const data = await res.json();
+  document.getElementById("nameBerrie").src= "../IMG/Bayas/\\"+data.name+".png";
   console.log(data.name);
 }
